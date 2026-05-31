60bc2ddd22f47c7fad603b7525feeafd6d9cfb57
hadielanshori347-alt <hadielanshori347@gmail.com>
2026-05-31 22:53:00 +0700

    Update config-mobile.js

--- config-mobile.js
+++ config-mobile.js
@@ -3,7 +3,7 @@
 // ════════════════════════════════════════════
 
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyx1C0PFBJCo458FQY0op6acd1ZHQsaSi_yQ32ZVuIcV4UKIdRKvjoHQ_7TGJX5GN7Blw/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzkEIivTT4yRw7aqPej_ADGht0FmCWU4E0VjUdwCEno_TukU93y_xWer2TFVe3zt4U08g/exec",
   DRIVE_FOLDER_ID: "1v95v5hZ9jvfudYUCIO3qP8XdOCH9dqgu",
   SUPABASE_URL: "https://twhtgiexupzwbycemdee.supabase.co",
   SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3aHRnaWV4dXB6d2J5Y2VtZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDE1NzQsImV4cCI6MjA5NTM3NzU3NH0.A-j3mbhZUbs8trZLRmYAWG0NP_UY3Jh2u8FyZ5_IOnw",
